18736f526adf4a9fe8bbebb73512d6ba16259395
Afel <162775751+afelallaric@users.noreply.github.com>
2024-10-01 00:15:33 +0700

    Update Main.java

--- MusicOrganizer/src/Main.java
+++ MusicOrganizer/src/Main.java
@@ -19,7 +19,7 @@ public class Main {
             System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
-            scanner.nextLine();  // consume newline
+            scanner.nextLine(); 
 
             switch (choice) {
                 case 1:
